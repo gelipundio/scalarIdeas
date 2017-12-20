19b1e5096f23aea1d82b85472db6066de1842a5b
Felipe Nevarez <wolf.antica@gmail.com>
2017-12-20 03:04:51 -0600

    changing title

--- count_down_timer/src/App.js
+++ count_down_timer/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
+          <h1 className='App-title'>Welcome to this awesome Count-Down-Timer!!</h1>
         </header>
         <p className='App-intro'>
           <Timer days={1} hours={23} minutes={27} seconds={47} />
